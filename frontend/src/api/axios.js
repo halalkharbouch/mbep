8d47e84bff5ffa7c90f97143f5546216a8dcc449
Halal Kharbouch <binyaminbgod@gmail.com>
2024-11-15 11:55:03 +0100

    scit commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const localIP = 'http://localhost:3000/api';
 const sharedIP = 'http://192.168.1.138:3000/api';
+const renderIP = 'https://mbep-backend.onrender.com/api'
 
 const axiosInstance = axios.create({
-  baseURL: localIP,
+  baseURL: renderIP,
 });
 
 export default axiosInstance;
